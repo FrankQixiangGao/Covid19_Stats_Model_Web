88d5697255e2abae438f4758c6d62477eb17f699
Frank Gao <frank.qixiang.gao@gmail.com>
2021-10-02 23:19:26 -0500

    progress 1

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ import numpy as np
 import pandas as pd
 from os.path import isfile
 from datetime import date
+import plotly.express as px
 from datetime import datetime as dt
 
 GlobalURL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/"
 fileNamePickle = "allData.pkl"
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-importdate = pd.read_csv("Covid19-Imp.csv")
+usaURL = "https://github.com/nytimes/covid-19-data/blob/master/us-counties.csv/"
+ImportPolicy = pd.read_csv("Covid19-Imp.csv")
+print(ImportPolicy)
 
 tickFont = {'size': 12, 'color': "rgb(30,30,30)", 'family': "Apple Chancery, cursive"}
 today = date.today()
@@ -25,7 +28,7 @@ def __loadData_GLOB(fileName, columnName):
         .astype({'date': 'datetime64[ns]', columnName: 'Int64'}, errors='ignore')
     data = data.groupby(['Country', 'date']).agg(agg_dict).reset_index()
     data['Province/State'] = '<all>'
-    return data
+    return pd.concat([data])
 
 
 def __loadData_US(fileName, columnName):
@@ -61,6 +64,7 @@ def allData():
     allData = pd.read_pickle(fileNamePickle)
     return allData
 
+
 countries = allData()['Country'].unique()
 countries.sort()
 
@@ -80,21 +84,20 @@ app.layout = html.Div(
                 )
             ]),
             html.Div(className="three columns", children=[
-                html.H5('State / Metropolis'),
+                html.H5('State / Province'),
                 dcc.Dropdown(
                     id='state'
                 )
             ]),
             html.Div(className="three columns", children=[
-                html.H5('county'),
+                html.H5('County'),
                 dcc.Dropdown(
-                    id='county'
+                    id='county',
                 )
             ]),
         ]),
         html.Div(className="row", children=[
             html.Div(className="nine columns", children=[
-                html.H5("Covid19 Time-Series Plot", style="center"),
                 dcc.Graph(
                     id="plot_new_metrics",
                     config={'displayModeBar': False}
@@ -111,6 +114,15 @@ app.layout = html.Div(
                     options=[{'label': m, 'value': m} for m in ['Confirmed', 'Deaths']],
                     value=['Confirmed', 'Deaths']
                 ),
+                html.H5('Selected Date'),
+                dcc.Checklist(
+                    id='important date',
+                    options=[{'label': m, 'value': m} for m in ['Policy Action date', 'Holiday']],
+                    value=['State Policy Action date', 'Holiday']
+                ),
+
+                html.Br(),
+                html.Br(),
                 html.H5("Calender"),
                 dcc.DatePickerRange(
                     id='my-date-picker-range',
@@ -129,8 +141,6 @@ app.layout = html.Div(
                     persistence_type='session',
                     updatemode="singledate"
                 ),
-                html.H5("Statistics Bar")
-
             ]),
         ]),
 
@@ -139,12 +149,19 @@ app.layout = html.Div(
             interval=3600 * 1000,  # Refresh data each hour.
             n_intervals=0
         ),
-       # html.Br(),
-       # html.Br(),
-       # html.Br(),
-      #  html.H5("Given credit to Frank and Yeyun for this web", style={'text-align': 'center'})
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        html.H5("Given credit to Frank and Yeyun for this web, and Yeyun is so cute!", style={'text-align': 'center'})
+
     ]
 )
+
 
 @app.callback(
     [Output('state', 'options'), Output('state', 'value')],
@@ -159,27 +176,42 @@ def update_states(country):
     state_value = state_options[0]['value']
     return state_options, state_value
 
-ImportantDate = []
-
+ImportDate = []
 def filtered_data(country, state):
+    global ImportDate
     d = allData()
     data = d.loc[d['Country'] == country].drop('Country', axis=1)
-    if state == '<all>':
+    if state == '<all>' or state is None:
         data = data.drop('Province/State', axis=1).groupby("date").sum().reset_index()
+        ImportDate = ["None", "2020-12-09", "2021-01-08", "2020-04-04", "2020-05-15"]
     else:
         data = data.loc[data['Province/State'] == state]
-        row = importdate.loc[importdate.iloc[:, 0] == state]
-        for date in row:
-            ImportantDate.append(date)
+        states = str(state)
+        row = ImportPolicy.loc[ImportPolicy["States"].str.match(states)]
+        vars = row.values.tolist()
+        i = range(5)
+        for n in i:
+            ImportDate.append(vars[0][n])
     newCases = data.select_dtypes(include='Int64').diff().fillna(0)
     newCases.columns = [column.replace('Cum', 'New') for column in newCases.columns]
     data = data.join(newCases)
     data['dateStr'] = data['date'].dt.strftime('%b %d, %Y')
     return data
 
+
+
 def barchart(data, metrics, start_date, end_date, prefix="", yaxisTitle=""):
+    global ImportDate
     start_date = dt.strptime(start_date, '%Y-%m-%d')
     end_date = dt.strptime(end_date, '%Y-%m-%d')
+    Mask = dt.strptime(ImportDate[1], '%Y-%m-%d')
+    Vaccine = dt.strptime(ImportDate[2], '%Y-%m-%d')
+    StayHome = dt.strptime(ImportDate[3], '%Y-%m-%d')
+    ReOpen = dt.strptime(ImportDate[4], '%Y-%m-%d')
+    fil_data = data[(data['date'] >= start_date) & (data['date'] <= end_date)]
+    mean = fil_data["NewConfirmed"].mean()
+    HighBound = fil_data["NewConfirmed"].max()
+    LowBound = fil_data["NewConfirmed"].min()
     figure = graph.Figure(data=[
         graph.Bar(
             name=metric, x=data.date, y=data[prefix + metric],
@@ -192,42 +224,45 @@ def barchart(data, metrics, start_date, end_date, prefix="", yaxisTitle=""):
         barmode='group',
         legend=dict(x=.05, y=0.95, font={'size': 15}, bgcolor='rgba(240,240,240,0.5)'),
         plot_bgcolor='#FFFFFF', font=tickFont) \
+        .add_vrect(x0=start_date, x1=end_date, y0=0, y1=10000, fillcolor="LightSalmon", opacity=0.3)\
         .update_xaxes(
-        title="mean = {}  daily confirm median = {}  daily confirm", tickangle=-90, type='category', showgrid=False, gridcolor='#DDDDDD',
+        title="mean = {mean:.0f} Highbound = {H:.0f} LowBound = {L:0f}".format(mean = mean, H=HighBound, L=LowBound), tickangle=-90, type='category', showgrid=False, gridcolor='#DDDDDD',
         tickfont=tickFont) \
         .update_yaxes(
         title=yaxisTitle, showgrid=True, gridcolor='#DDDDDD') \
-        .add_vrect(x0=start_date, x1=end_date, y0=0, y1=1, fillcolor="LightSalmon", opacity=0.3) \
-        .add_shape(type="line", x0=Vaccine, x1=Vaccine,
+        .add_annotation(x=dt(2020, 7, 4), y=10001,
+                    text="Independent Day",
+                    showarrow=True,
+                    arrowhead=1) \
+        .add_shape(type="line", x0=dt(2021, 11, 5), x1=dt(2021, 11, 5), y0=10000, y1=0, line=dict(color="Blue", width=1)) \
+        .add_annotation(x=dt(2021, 11, 5), y=10001,
+                    text="Labor Day",
+                    showarrow=True,
+                    arrowhead=1) \
+        .add_shape(type="line", x0=Mask, x1=Mask, y0=10000, y1=0,
+                   line=dict(color="Purple", width=1)) \
+        .add_annotation(x=Mask, y=10001,
+                        text="Mask Start",
+                        showarrow=True,
+                        arrowhead=1) \
+        .add_shape(type="line", x0=Vaccine, x1=Vaccine, y0=10000, y1=0,
                    line=dict(color="Green", width=1)) \
-        .add_annotation(x=Vaccine,
-                        hovertext="Vaccine",
+        .add_annotation(x=Vaccine, y=10001,
+                        text="Vaccine Start",
                         showarrow=True,
                         arrowhead=1) \
-        .add_vline(x="2020-07-04", line_width=3, line_dash="dash", line_color="purple") \
-        .add_shape(type="line", x0=dt(2020, 12, 25), x1=dt(2020, 12, 25), y0=1000000, y1=0, line=dict(
-        color="MediumPurple", width=1)) \
-        .add_annotation(x=dt(2020, 12, 25), y=1000001,
-                        hovertext="Christmas",
+        .add_shape(type="line", x0=StayHome, x1=StayHome, y0=10000, y1=0,
+                   line=dict(color="Yellow", width=1)) \
+        .add_annotation(x=StayHome, y=10001,
+                        text="Stay Home",
                         showarrow=True,
                         arrowhead=1) \
-        .add_shape(type="line", x0=dt(2020, 7, 4), x1=dt(2020, 7, 4), y0=1000000, y1=0, line=dict(
-        color="Yellow", width=1)) \
-        .add_annotation(x=dt(2020, 7, 4), y=1000001,
-                        hovertext="Independent Day",
+        .add_shape(type="line", x0=ReOpen, x1=ReOpen, y0=10000, y1=0,
+               line=dict(color="Blue", width=1)) \
+        .add_annotation(x=ReOpen, y=10001,
+                        text="ReOpen",
                         showarrow=True,
-                        arrowhead=1) \
-        .add_shape(type="line", x0=dt(2021, 7, 4), x1=dt(2021, 7, 4), y0=1000000, y1=0, line=dict(
-        color="Yellow", width=1)) \
-        .add_annotation(x=dt(2021, 7, 4), y=1000001,
-                        hovertext="Independent Day",
-                        arrowhead=1) \
-        .add_shape(type="line", x0=dt(2021, 11, 5), x1=dt(2021, 11, 5), y0=1000000, y1=0,
-                   line=dict(color="Blue", width=1)) \
-        .add_annotation(x=dt(2021, 11, 5), y=1000001,
-                        showarrow=True,
-                        arrowhead=1,
-                        hovertext="Labor Day") \
+                        arrowhead=1)
 
     return figure
 
@@ -236,7 +271,7 @@ def barchart(data, metrics, start_date, end_date, prefix="", yaxisTitle=""):
     [Output('plot_new_metrics', 'figure'), Output('plot_cum_metrics', 'figure')],
     [Input('country', 'value'), Input('state', 'value'), Input('metrics', 'value'),
      Input('interval-component', 'n_intervals'), Input("my-date-picker-range", "start_date"),
-     Input("my-date-picker-range", "end_date"),]
+     Input("my-date-picker-range", "end_date")]
 )
 def update_plots(country, state, metrics, n, start_date, end_date):
     refreshData()
